ed383c5a56f907ced4fec684db54d2d302a0bccb
karmouah <karmouah@getalp-athena3.imag.fr>
2024-05-27 14:16:55 +0200

    done with the third and fourth part

--- tuto_mlflow.py
+++ tuto_mlflow.py
@@ -17,4 +17,32 @@ default_experiment = [
     if experiment.name == "Default"
 ][0]
 
-pprint(default_experiment)
+
+# Provide an Experiment description that will appear in the UI
+experiment_description = (
+    "This is the grocery forecasting project. "
+    "This experiment contains the produce models for apples."
+)
+
+
+# Provide searchable tags that define characteristics of the Runs that
+# will be in this Experiment
+experiment_tags = {
+    "project_name": "grocery-forecasting",
+    "store_dept": "produce",
+    "team": "stores-ml",
+    "project_quarter": "Q3-2023",
+    "mlflow.note.content": experiment_description
+}
+
+# Create the Experiment, providing a unique name
+produce_apples_experiment = client.create_experiment(
+    name="Apple_Models", tags=experiment_tags
+)
+
+# Use search_experiments() to search on the project_name tag key
+apples_experiment = client.search_experiments(
+    filter_string="tags.`project_name`= 'grocery-forecasting'"
+)
+
+pprint(vars(apples_experiment[0]))
