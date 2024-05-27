019290d4bb200e489e3ec88d79660de2014dc765
karmouah <karmouah@getalp-athena3.imag.fr>
2024-05-27 11:57:19 +0200

    done with the second part of the tutorial

--- tuto_mlflow.py
+++ tuto_mlflow.py
@@ -8,4 +8,13 @@ client = MlflowClient(tracking_uri="http://127.0.0.1:8080")
 
 # searcing experiments (one exists by default, it encapsulated 
 # run information if an explicit Experiment is not declared)
+all_experiments = client.search_experiments()
 
+# accessing elements from all_experiments and getting the first one that has the name "Default"
+default_experiment = [
+    {"name": experiment.name, "lifecycle_stage":experiment.lifecycle_stage}
+    for experiment in all_experiments
+    if experiment.name == "Default"
+][0]
+
+pprint(default_experiment)
