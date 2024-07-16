28b9682eebe1beb625cc56aecf93d40bc143b6ce
karmouah <karmouah@getalp-athena3.imag.fr>
2024-07-16 09:04:45 +0200

    added a function to empty the db before saving new experiments to it

--- remote_tracking/solo_development/import_from_db.py
+++ remote_tracking/solo_development/import_from_db.py
@@ -3,102 +3,125 @@ import psycopg2
 from prettytable import PrettyTable
 from mlflow_entities import Experiment, Run, Param, Metric, Tag
 
-
-
 # URL to connect to the db
 DB_URL = "postgresql://user1:password1@localhost:5433/mlflowdb1"
 
-# Id of the experiment we want to add (we can pass it as an argument)
-EXPERIMENT_ID = "1"
- 
-def get_experiment_from_db():
+def get_all_experiments_from_db():
     # Connect to the db
     conn = psycopg2.connect(DB_URL)
-
     cursor = conn.cursor()
 
-
-    # Récuperer les données de l'experience
-    cursor.execute(f"""
-        SELECT name, artifact_location
-        FROM experiments
-        WHERE experiment_id = {EXPERIMENT_ID}
-    """)
-
-    experiment_data = cursor.fetchone()
-
-    if experiment_data is None:
-            cursor.close()
-            conn.close()
-            raise ValueError(f"No experiment found with id {experiment_id}")
-        
-    name, artifact_location = experiment_data
-    experiment = Experiment(name, artifact_location)
-
-
-
-    # Récupérer les runs associés à l'expérience
-    cursor.execute(f"""
-        SELECT run_uuid, name, experiment_id, start_time, end_time, status
-        FROM runs
-        WHERE experiment_id = {EXPERIMENT_ID}
-    """)
-    runs_data = cursor.fetchall()
+    # Récupérer toutes les expériences
+    cursor.execute("SELECT experiment_id, name, artifact_location FROM experiments")
+    experiments_data = cursor.fetchall()
     
-    for row in runs_data:
-        run_id, name, experiment_id, start_time, end_time, status = row
-        run = Run(name, experiment_id,start_time, end_time, status)
+    experiments = []
+    
+    for exp_data in experiments_data:
+        experiment_id, name, artifact_location = exp_data
+        experiment = Experiment(name, artifact_location)
         
-        # Récupérer les paramètres pour chaque run
+        # Récupérer les runs associés à l'expérience
         cursor.execute(f"""
-            SELECT run_uuid, key, value
-            FROM params
-            WHERE run_uuid = '{run_id}'
+            SELECT run_uuid, name, experiment_id, start_time, end_time, status
+            FROM runs
+            WHERE experiment_id = {experiment_id}
         """)
-        params_data = cursor.fetchall()
-        for param_row in params_data:
-            _, key, value = param_row
-            param = Param(key, value)
-            run.add_param(param)
+        runs_data = cursor.fetchall()
         
-        # Récupérer les métriques pour chaque run
-        cursor.execute(f"""
-            SELECT run_uuid, key, value, timestamp, step
-            FROM metrics
-            WHERE run_uuid = '{run_id}'
-        """)
-        metrics_data = cursor.fetchall()
-        for metric_row in metrics_data:
-            _, key, value, timestamp, step = metric_row
-            metric = Metric(key, value, timestamp, step)
-            run.add_metric(metric)
+        for row in runs_data:
+            run_id, name, experiment_id, start_time, end_time, status = row
+            run = Run(name, experiment_id, start_time, end_time, status)
+            
+            # Récupérer les paramètres pour chaque run
+            cursor.execute(f"""
+                SELECT run_uuid, key, value
+                FROM params
+                WHERE run_uuid = '{run_id}'
+            """)
+            params_data = cursor.fetchall()
+            for param_row in params_data:
+                _, key, value = param_row
+                param = Param(key, value)
+                run.add_param(param)
+            
+            # Récupérer les métriques pour chaque run
+            cursor.execute(f"""
+                SELECT run_uuid, key, value, timestamp, step
+                FROM metrics
+                WHERE run_uuid = '{run_id}'
+            """)
+            metrics_data = cursor.fetchall()
+            for metric_row in metrics_data:
+                _, key, value, timestamp, step = metric_row
+                metric = Metric(key, value, timestamp, step)
+                run.add_metric(metric)
+            
+            # Récupérer les tags pour chaque run
+            cursor.execute(f"""
+                SELECT run_uuid, key, value
+                FROM tags
+                WHERE run_uuid = '{run_id}'
+            """)
+            tags_data = cursor.fetchall()
+            for tag_row in tags_data:
+                _, key, value = tag_row
+                tag = Tag(key, value)
+                run.add_tag(tag)
+            
+            experiment.add_run(run)
         
-        # Récupérer les tags pour chaque run
-        cursor.execute(f"""
-            SELECT run_uuid, key, value
-            FROM tags
-            WHERE run_uuid = '{run_id}'
-        """)
-        tags_data = cursor.fetchall()
-        for tag_row in tags_data:
-            _, key, value = tag_row
-            tag = Tag(key, value)
-            run.add_tag(tag)
-        
-        experiment.add_run(run)
+        experiments.append(experiment)
 
     # Fermeture du curseur et de la connexion à la base de données
     cursor.close()
     conn.close()
     
-    return experiment
-
-
-
-
+    return experiments
 
 
 SHARED_DB_URL = "postgresql://user:password@localhost:5432/mlflowdb"
+
+
+
+def delete_all_experiments_from_db():
+    try:
+        # Connexion à la base de données PostgreSQL
+        conn = psycopg2.connect(SHARED_DB_URL)
+        cursor = conn.cursor()
+
+        # Supprimer les paramètres
+        cursor.execute("DELETE FROM params")
+        
+        # Supprimer les métriques
+        cursor.execute("DELETE FROM metrics")
+        
+        # Supprimer les tags
+        cursor.execute("DELETE FROM tags")
+
+        cursor.execute("DELETE FROM latest_metrics")
+        
+        # Supprimer les runs
+        cursor.execute("DELETE FROM runs")
+
+        cursor.execute("DELETE FROM datasets")
+        
+        # Supprimer les expériences
+        cursor.execute("DELETE FROM experiments")
+        
+        # Valider et appliquer les changements
+        conn.commit()
+        print("All experiments and associated data have been deleted from the database.")
+
+    except (Exception, psycopg2.Error) as error:
+        print(f"Error while deleting all experiments: {error}")
+
+    finally:
+        # Fermer la connexion
+        if conn:
+            cursor.close()
+            conn.close()
+
 
 def delete_experiment_from_db(experiment_name):
     try:
@@ -135,17 +158,20 @@ def delete_experiment_from_db(experiment_name):
             conn.close()
 
 
-
 if __name__ == '__main__':
-    delete_experiment_from_db('experiment1') # add "if exists"
+    # delete_experiment_from_db('experiment1') # add "if exists"
     mlflow.set_tracking_uri("http://127.0.0.1:5000")
-    experiment = get_experiment_from_db()
-    server_experiment_id = mlflow.create_experiment(experiment.name)
-    for run in experiment.runs:
-        with mlflow.start_run(experiment_id = server_experiment_id, run_name = run.name):
-            for param in run.params:
-                mlflow.log_param(param.key, param.value)
-            for metric in run.metrics:
-                mlflow.log_metric(metric.key, metric.value)
-            for tag in run.tags:
-                mlflow.set_tag(tag.key, tag.value)
+    experiments = get_all_experiments_from_db()
+    for experiment in experiments:
+        if experiment.name != "Default":
+            server_experiment_id = mlflow.create_experiment(experiment.name)
+        else:
+            server_experiment_id = '0'
+        for run in experiment.runs:
+            with mlflow.start_run(experiment_id = server_experiment_id, run_name = run.name):
+                for param in run.params:
+                    mlflow.log_param(param.key, param.value)
+                for metric in run.metrics:
+                    mlflow.log_metric(metric.key, metric.value)
+                for tag in run.tags:
+                    mlflow.set_tag(tag.key, tag.value)
--- remote_tracking/solo_development/script.py
+++ remote_tracking/solo_development/script.py
@@ -1,13 +1,62 @@
 import mlflow
+import psycopg2
 
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_diabetes
 from sklearn.ensemble import RandomForestRegressor
 
+SHARED_DB_URL = "postgresql://user1:password1@localhost:5433/mlflowdb1"
+SOLO_DB_URL = "postgresql://user:password@localhost:5432/mlflowdb"
+
+
+def delete_all_experiments_from_db(DB_URL):
+    try:
+        # Connexion à la base de données PostgreSQL
+        conn = psycopg2.connect(DB_URL)
+        cursor = conn.cursor()
+
+        # Supprimer les paramètres
+        cursor.execute("DELETE FROM params")
+        
+        # Supprimer les métriques
+        cursor.execute("DELETE FROM metrics")
+        
+        # Supprimer les tags
+        cursor.execute("DELETE FROM tags")
+
+        cursor.execute("DELETE FROM latest_metrics")
+        
+        # Supprimer les runs
+        cursor.execute("DELETE FROM runs")
+
+        cursor.execute("DELETE FROM datasets")
+        
+        # Supprimer les expériences
+        cursor.execute("DELETE FROM experiments")
+        
+        # Valider et appliquer les changements
+        conn.commit()
+        print("All experiments and associated data have been deleted from the database.")
+
+    except (Exception, psycopg2.Error) as error:
+        print(f"Error while deleting all experiments: {error}")
+
+    finally:
+        # Fermer la connexion
+        if conn:
+            cursor.close()
+            conn.close()
+
+
+
+# On vide la base à chaque fois pour ne pas ajouter des exp déjà existantes sur le serveur commun
+delete_all_experiments_from_db(SHARED_DB_URL)
+delete_all_experiments_from_db(SOLO_DB_URL)
+
 
 mlflow.set_tracking_uri("postgresql://user1:password1@localhost:5433/mlflowdb1")
 
-mlflow.set_experiment("experiment1")
+mlflow.set_experiment("experiment_test_1")
 
 mlflow.sklearn.autolog()
 
